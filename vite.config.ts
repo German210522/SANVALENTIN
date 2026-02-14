7608ff2fea47f14cd149f8b0084f43d609c94c1a
German Juárez <juarezsilvety@gmail.com>
2026-02-14 16:02:49 -0600

    Activar despliegue automático Jz.Dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  // Esto detecta si estás en GitHub Pages o en tu PC local
-  base: process.env.NODE_ENV === 'production' ? '/SANVALENTIN/' : '/',
+  base: '/SANVALENTIN/',
 })
